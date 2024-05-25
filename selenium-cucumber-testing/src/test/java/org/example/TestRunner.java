ee6d2144445c834a0fb234af93a43c085b63af1e
Adriano Díaz Benítez <adrianodiazb@gmail.com>
2024-05-25 20:13:35 +0200

    added cucumber options

--- selenium-cucumber-testing/src/test/java/org/example/TestRunner.java
+++ selenium-cucumber-testing/src/test/java/org/example/TestRunner.java
@@ -1,4 +1,13 @@
 package org.example;
 
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "features",
+        glue = "steps"
+)
 public class TestRunner {
 }
